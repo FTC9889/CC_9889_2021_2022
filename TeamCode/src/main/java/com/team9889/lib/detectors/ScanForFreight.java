5ab4f734514365b3f4d0b427b930714cabe04247
Eric <em2004junk@gmail.com>
2021-12-23 14:45:18 -0500

    12/23 Early

---
+++ TeamCode/src/main/java/com/team9889/lib/detectors/ScanForFreight.java
@@ -0,0 +1,362 @@
+package com.team9889.lib.detectors;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.team9889.lib.detectors.util.HSV;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Eric on 12/18/2021.
+ */
+
+@Config
+public class ScanForFreight extends OpenCvPipeline {
+
+    //Outputs
+    private Mat resizeImageOutput = new Mat();
+    private Mat blurOutput = new Mat();
+    private Mat hsvThreshold0Output = new Mat();
+    private Mat hsvThreshold1Output = new Mat();
+    private Mat cvBitwiseOrOutput = new Mat();
+    private Mat maskOutput = new Mat();
+    private ArrayList<MatOfPoint> findContoursOutput = new ArrayList<MatOfPoint>();
+    private ArrayList<MatOfPoint> filterContoursOutput = new ArrayList<MatOfPoint>();
+
+    public static HSV hsv = new HSV(1.618705035971223, 34.16400503732365,
+            171.0157297890501, 255.0, 191.4123785717189, 255.0);
+
+//    static {
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//    }
+
+    /**
+     * This is the primary method that runs the entire pipeline and updates the outputs.
+     */
+    @Override
+    public Mat processFrame(Mat input) {
+        // Step Resize_Image0:
+        Mat resizeImageInput = input;
+        double resizeImageWidth = 320.0;
+        double resizeImageHeight = 240.0;
+        int resizeImageInterpolation = Imgproc.INTER_CUBIC;
+        resizeImage(resizeImageInput, resizeImageWidth, resizeImageHeight, resizeImageInterpolation, resizeImageOutput);
+
+        // Step Blur0:
+        Mat blurInput = resizeImageOutput;
+        BlurType blurType = BlurType.get("Box Blur");
+        double blurRadius = 1.8018018018018018;
+        blur(blurInput, blurType, blurRadius, blurOutput);
+
+        // Step HSV_Threshold0:
+        Mat hsvThreshold0Input = blurOutput;
+        hsvThreshold(hsvThreshold0Input, hsv.getH(), hsv.getS(), hsv.getV(), hsvThreshold0Output);
+
+        // Step HSV_Threshold1:
+        Mat hsvThreshold1Input = blurOutput;
+        double[] hsvThreshold1Hue = {0.0, 46.844919786096256};
+        double[] hsvThreshold1Saturation = {0.0, 105.0};
+        double[] hsvThreshold1Value = {189.68926553672316, 255.0};
+        hsvThreshold(hsvThreshold1Input, hsvThreshold1Hue, hsvThreshold1Saturation, hsvThreshold1Value, hsvThreshold1Output);
+
+        // Step CV_bitwise_or0:
+        Mat cvBitwiseOrSrc1 = hsvThreshold0Output;
+        Mat cvBitwiseOrSrc2 = hsvThreshold1Output;
+        cvBitwiseOr(cvBitwiseOrSrc1, cvBitwiseOrSrc2, cvBitwiseOrOutput);
+
+        // Step Mask0:
+        Mat maskInput = blurOutput;
+        Mat maskMask = cvBitwiseOrOutput;
+        mask(maskInput, maskMask, maskOutput);
+
+        // Step Find_Contours0:
+        Mat findContoursInput = cvBitwiseOrOutput;
+        boolean findContoursExternalOnly = false;
+        findContours(findContoursInput, findContoursExternalOnly, findContoursOutput);
+
+        // Step Filter_Contours0:
+        ArrayList<MatOfPoint> filterContoursContours = findContoursOutput;
+        double filterContoursMinArea = 150.0;
+        double filterContoursMinPerimeter = 0.0;
+        double filterContoursMinWidth = 0.0;
+        double filterContoursMaxWidth = 1000;
+        double filterContoursMinHeight = 0;
+        double filterContoursMaxHeight = 1000;
+        double[] filterContoursSolidity = {0.0, 100.0};
+        double filterContoursMaxVertices = 10000.0;
+        double filterContoursMinVertices = 0.0;
+        double filterContoursMinRatio = 0;
+        double filterContoursMaxRatio = 1000;
+        filterContours(filterContoursContours, filterContoursMinArea, filterContoursMinPerimeter, filterContoursMinWidth, filterContoursMaxWidth, filterContoursMinHeight, filterContoursMaxHeight, filterContoursSolidity, filterContoursMaxVertices, filterContoursMinVertices, filterContoursMinRatio, filterContoursMaxRatio, filterContoursOutput);
+
+        for (int i = 0; i < filterContoursOutput.size(); i++) {
+            Imgproc.drawContours(blurOutput, filterContoursOutput, i, new Scalar(255, 0, 0));
+        }
+
+        return blurOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Resize_Image.
+     * @return Mat output from Resize_Image.
+     */
+    public Mat resizeImageOutput() {
+        return resizeImageOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Blur.
+     * @return Mat output from Blur.
+     */
+    public Mat blurOutput() {
+        return blurOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a HSV_Threshold.
+     * @return Mat output from HSV_Threshold.
+     */
+    public Mat hsvThreshold0Output() {
+        return hsvThreshold0Output;
+    }
+
+    /**
+     * This method is a generated getter for the output of a HSV_Threshold.
+     * @return Mat output from HSV_Threshold.
+     */
+    public Mat hsvThreshold1Output() {
+        return hsvThreshold1Output;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_bitwise_or.
+     * @return Mat output from CV_bitwise_or.
+     */
+    public Mat cvBitwiseOrOutput() {
+        return cvBitwiseOrOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Mask.
+     * @return Mat output from Mask.
+     */
+    public Mat maskOutput() {
+        return maskOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Find_Contours.
+     * @return ArrayList<MatOfPoint> output from Find_Contours.
+     */
+    public ArrayList<MatOfPoint> findContoursOutput() {
+        return findContoursOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a Filter_Contours.
+     * @return ArrayList<MatOfPoint> output from Filter_Contours.
+     */
+    public ArrayList<MatOfPoint> filterContoursOutput() {
+        return filterContoursOutput;
+    }
+
+
+    /**
+     * Scales and image to an exact size.
+     * @param input The image on which to perform the Resize.
+     * @param width The width of the output in pixels.
+     * @param height The height of the output in pixels.
+     * @param interpolation The type of interpolation.
+     * @param output The image in which to store the output.
+     */
+    private void resizeImage(Mat input, double width, double height,
+                             int interpolation, Mat output) {
+        Imgproc.resize(input, output, new Size(width, height), 0.0, 0.0, interpolation);
+    }
+
+    /**
+     * An indication of which type of filter to use for a blur.
+     * Choices are BOX, GAUSSIAN, MEDIAN, and BILATERAL
+     */
+    enum BlurType{
+        BOX("Box Blur"), GAUSSIAN("Gaussian Blur"), MEDIAN("Median Filter"),
+        BILATERAL("Bilateral Filter");
+
+        private final String label;
+
+        BlurType(String label) {
+            this.label = label;
+        }
+
+        public static BlurType get(String type) {
+            if (BILATERAL.label.equals(type)) {
+                return BILATERAL;
+            }
+            else if (GAUSSIAN.label.equals(type)) {
+                return GAUSSIAN;
+            }
+            else if (MEDIAN.label.equals(type)) {
+                return MEDIAN;
+            }
+            else {
+                return BOX;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return this.label;
+        }
+    }
+
+    /**
+     * Softens an image using one of several filters.
+     * @param input The image on which to perform the blur.
+     * @param type The blurType to perform.
+     * @param doubleRadius The radius for the blur.
+     * @param output The image in which to store the output.
+     */
+    private void blur(Mat input, BlurType type, double doubleRadius,
+                      Mat output) {
+        int radius = (int)(doubleRadius + 0.5);
+        int kernelSize;
+        switch(type){
+            case BOX:
+                kernelSize = 2 * radius + 1;
+                Imgproc.blur(input, output, new Size(kernelSize, kernelSize));
+                break;
+            case GAUSSIAN:
+                kernelSize = 6 * radius + 1;
+                Imgproc.GaussianBlur(input,output, new Size(kernelSize, kernelSize), radius);
+                break;
+            case MEDIAN:
+                kernelSize = 2 * radius + 1;
+                Imgproc.medianBlur(input, output, kernelSize);
+                break;
+            case BILATERAL:
+                Imgproc.bilateralFilter(input, output, -1, radius, radius);
+                break;
+        }
+    }
+
+    /**
+     * Segment an image based on hue, saturation, and value ranges.
+     *
+     * @param input The image on which to perform the HSL threshold.
+     * @param hue The min and max hue
+     * @param sat The min and max saturation
+     * @param val The min and max value
+//     * @param output The image in which to store the output.
+     */
+    private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
+                              Mat out) {
+        Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
+        Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
+                new Scalar(hue[1], sat[1], val[1]), out);
+    }
+
+    /**
+     * Computes the per channel or of two images.
+     * @param src1 The first image to use.
+     * @param src2 The second image to use.
+     * @param dst the result image when the or is performed.
+     */
+    private void cvBitwiseOr(Mat src1, Mat src2, Mat dst) {
+        Core.bitwise_or(src1, src2, dst);
+    }
+
+    /**
+     * Filter out an area of an image using a binary mask.
+     * @param input The image on which the mask filters.
+     * @param mask The binary image that is used to filter.
+     * @param output The image in which to store the output.
+     */
+    private void mask(Mat input, Mat mask, Mat output) {
+        mask.convertTo(mask, CvType.CV_8UC1);
+        Core.bitwise_xor(output, output, output);
+        input.copyTo(output, mask);
+    }
+
+    /**
+     * Sets the values of pixels in a binary image to their distance to the nearest black pixel.
+     * @param input The image on which to perform the Distance Transform.
+//     * @param type The Transform.
+//     * @param maskSize the size of the mask.
+//     * @param output The image in which to store the output.
+     */
+    private void findContours(Mat input, boolean externalOnly,
+                              List<MatOfPoint> contours) {
+        Mat hierarchy = new Mat();
+        contours.clear();
+        int mode;
+        if (externalOnly) {
+            mode = Imgproc.RETR_EXTERNAL;
+        }
+        else {
+            mode = Imgproc.RETR_LIST;
+        }
+        int method = Imgproc.CHAIN_APPROX_SIMPLE;
+        Imgproc.findContours(input, contours, hierarchy, mode, method);
+    }
+
+
+    /**
+     * Filters out contours that do not meet certain criteria.
+     * @param inputContours is the input list of contours
+     * @param output is the the output list of contours
+     * @param minArea is the minimum area of a contour that will be kept
+     * @param minPerimeter is the minimum perimeter of a contour that will be kept
+     * @param minWidth minimum width of a contour
+     * @param maxWidth maximum width
+     * @param minHeight minimum height
+     * @param maxHeight maximimum height
+//     * @param Solidity the minimum and maximum solidity of a contour
+     * @param minVertexCount minimum vertex Count of the contours
+     * @param maxVertexCount maximum vertex Count
+     * @param minRatio minimum ratio of width to height
+     * @param maxRatio maximum ratio of width to height
+     */
+    private void filterContours(List<MatOfPoint> inputContours, double minArea,
+                                double minPerimeter, double minWidth, double maxWidth, double minHeight, double
+                                        maxHeight, double[] solidity, double maxVertexCount, double minVertexCount, double
+                                        minRatio, double maxRatio, List<MatOfPoint> output) {
+        final MatOfInt hull = new MatOfInt();
+        output.clear();
+        //operation
+        for (int i = 0; i < inputContours.size(); i++) {
+            final MatOfPoint contour = inputContours.get(i);
+            final Rect bb = Imgproc.boundingRect(contour);
+            if (bb.width < minWidth || bb.width > maxWidth) continue;
+            if (bb.height < minHeight || bb.height > maxHeight) continue;
+            final double area = Imgproc.contourArea(contour);
+            if (area < minArea) continue;
+            if (Imgproc.arcLength(new MatOfPoint2f(contour.toArray()), true) < minPerimeter) continue;
+            Imgproc.convexHull(contour, hull);
+            MatOfPoint mopHull = new MatOfPoint();
+            mopHull.create((int) hull.size().height, 1, CvType.CV_32SC2);
+            for (int j = 0; j < hull.size().height; j++) {
+                int index = (int)hull.get(j, 0)[0];
+                double[] point = new double[] { contour.get(index, 0)[0], contour.get(index, 0)[1]};
+                mopHull.put(j, 0, point);
+            }
+            final double solid = 100 * area / Imgproc.contourArea(mopHull);
+            if (solid < solidity[0] || solid > solidity[1]) continue;
+            if (contour.rows() < minVertexCount || contour.rows() > maxVertexCount)	continue;
+            final double ratio = bb.width / (double)bb.height;
+            if (ratio < minRatio || ratio > maxRatio) continue;
+            output.add(contour);
+        }
+    }
+}
